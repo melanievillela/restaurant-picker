a0e97262d46ef48d949fa975795721d545df8d7a
Melanie Villela <melanienvillela@gmail.com>
2018-12-13 16:01:49 -0600

    Update
    Added pickle module so that the user input to add a restaurant can be saved.
    
    Finished options to either view, add or choose a random restaurant.

--- restaurant-picker.py
+++ restaurant-picker.py
@@ -1,45 +1,63 @@
-restaurants = {
-    "Cheddars" : "American",
-    "Arandas" : "Mexican",
-    "Gyro House" : "Greek",
-    "Catfish Station" : "Seafood",
-    "CT Banh Mi" : "Vietnamese",
-    "The Catch" : "Seafood",
-    "Pho & More" : "Vietnamese",
-    "Bonsai Fusion" : "Japanese",
-    "Mamacitas" : "Mexican",
-    "Chilis" : "American"
-}
+import random
+import pickle
+#Get the dictionary of restaurants
+with open("restaurant_list.pkl", "rb") as pickle_file:
+    restaurants = pickle.load(pickle_file)
 
-def welcome_options():
-    global original_length 
-    original_length = len(restaurants)
-    print("Welcome to the Restaurant Picker!")
+def welcome_options():    
     print("Would you like to:\n   1) View the list of restaurants\n   2) Add a new restaurant to the list\n   3) Choose a random restaurant to eat at? ")
-    print("Please type in 1, 2 or 3")
+    print("Please type in 1, 2 or 3 or write esc to exit")
     global answer 
     answer = input()
-    return answer
     user_options()
 
 def user_options():
     if answer == "1":
+        #Show list of restaurants
         for restaurant in restaurants:
             print (restaurant)
         welcome_options()
     elif answer == "2":
+        #Add restaurant to dictionary
+        original_length = len(restaurants)
         print("Restaurant Name: ")
         name = input()
-        print("Restaurant Cuisine (American, Mexican, Seafood etc...) ")
-        cuisine = input()
-        restaurants[name] = cuisine
-        new_length = len(restaurants)
-        if new_length > original_length:
-            print("Restaurant added!")
-            welcome_options()
-        else:
-            print("Restaurant not added!")
+        if name in restaurants:
+            print("That restaurant already exists in your list of restaurants.")
+            user_options()
+        else: 
+            print("Restaurant Cuisine (American, Mexican, Seafood etc...) ")
+            cuisine = input()
+            #Add new restaurant into dictionary
+            restaurants[name] = cuisine
+            #Save dictionary
+            with open("restaurant_list.pkl", "wb") as pickle_file:
+                pickle.dump(restaurants, pickle_file)            
+            new_length = len(restaurants)
+            if new_length > original_length:
+                print("Restaurant added!")
+                welcome_options()
+            else:
+                print("Restaurant not added! Try again.")
+                welcome_options()
+    elif answer == "3":
+        #Get a random restaurant
+        randomize_restaurants()
+        welcome_options()
+    elif answer == "esc":
+        exit()
     else:
-        print (answer)
+        print("Sorry that is not an option please try again!")
+        welcome_options()
 
+def randomize_restaurants():
+    #Turn dictionary into array to use random int
+    random_restaurant_list = []
+    for restaurant in restaurants:
+        random_restaurant_list.append(restaurant)
+    length = len(restaurants)
+    random_restaurant = random.randint(1,length)    
+    print(random_restaurant_list[random_restaurant])
+
+print("Welcome to the Restaurant Picker!")
 welcome_options()
